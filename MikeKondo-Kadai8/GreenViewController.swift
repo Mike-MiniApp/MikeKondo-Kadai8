5cebe7d576a6c47c552de0f85943d070fdc84cd4
mikekondo <foad5969@gmail.com>
2022-08-09 06:42:59 +0900

    提出用

---
+++ MikeKondo-Kadai8/GreenViewController.swift
@@ -0,0 +1,36 @@
+//
+//  GreenViewController.swift
+//  MikeKondo-Kadai8
+//
+//  Created by 近藤米功 on 2022/08/08.
+//
+
+import UIKit
+
+final class GreenViewController: UIViewController {
+    // MARK: - UI Parts
+    @IBOutlet private weak var sliderLabel: UILabel!
+    @IBOutlet private weak var slider: UISlider!
+    private var sliderValue: Float = 0
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupSlider()
+    }
+
+    @IBAction private func didSlideSlider(_ sender: Any) {
+        updateSliderValue()
+    }
+
+    private func setupSlider() {
+        guard let sliderValue = UserDefaults.standard.object(forKey: "sliderValueKey") as? Float else { return }
+        slider.value = sliderValue
+        sliderLabel.text = String(sliderValue)
+    }
+
+    private func updateSliderValue() {
+        sliderValue = slider.value
+        sliderLabel.text = String(sliderValue)
+        UserDefaults.standard.setValue(sliderValue, forKey: "sliderValueKey")
+    }
+}
